e22b47fd6ae1b8393cf8ca4e7ce0f9ce0e09681a
fegomezl <fegomezl@unal.edu.co>
2022-05-15 22:42:10 -0500

    simplified ifs for time step

--- code/time_step.cpp
+++ code/time_step.cpp
@@ -19,83 +19,79 @@ double system_evolve(const Config &config, Crandom &random, std::vector<bool> &d
      *
      ****/ 
 
+    double x=0., y=0.;
+    int x0=0, x1=0, y0=0, y1=0;
+    double Ex=0., Ey=0.;
     auto particles_old = particles;
 
+    int k = 0;
+    bool liquid = 0;
+    double V_new = 0.;
+    std::vector<int> neighbors = {0, 0, 0, 0};
+
     for(long unsigned int ii = 0; ii < particles.size()/2; ii++){
-        double x = particles[2*ii]/config.l;
-        double y = particles[2*ii+1]/config.l;
-        int x0 = floor(x);
-        int x1 = x0 + 1;
-        int y0 = floor(y);
-        int y1 = y0 + 1;
+        x = particles[2*ii]/config.l;
+        y = particles[2*ii+1]/config.l;
+        x0 = floor(x);
+        x1 = x0 + 1;
+        y0 = floor(y);
+        y1 = y0 + 1;
 
-        double Ex00 = 0., Ex01 = 0., Ex10 = 0., Ex11 = 0.;
-        double Ey00 = 0., Ey01 = 0., Ey10 = 0., Ey11 = 0.;
-
-
-        //Estos 4 if son mas rapidos que los 2 if con 2 if dentro 
-        if (abs(x0) <= (config.n - 1)/2 && abs(y0) <= (config.n - 1)/2){
-            Ex00 = electric_field[0][x0+(config.n-1)/2 + (y0+(config.n-1)/2)*config.n];
-            Ey00 = electric_field[1][x0+(config.n-1)/2 + (y0+(config.n-1)/2)*config.n];
+        if (((1-2*std::signbit(x0))*(x0+x1) <= config.n - 2) && ((1-2*std::signbit(y0))*(y0+y1) <= config.n - 2)){
+            Ex = (x1-x)*(y1-y)*electric_field[0][x0+(config.n-1)/2 + (y0+(config.n-1)/2)*config.n]
+               + (x1-x)*(y-y0)*electric_field[0][x0+(config.n-1)/2 + (y1+(config.n-1)/2)*config.n]
+               + (x-x0)*(y1-y)*electric_field[0][x1+(config.n-1)/2 + (y0+(config.n-1)/2)*config.n]
+               + (x-x0)*(y-y0)*electric_field[0][x1+(config.n-1)/2 + (y1+(config.n-1)/2)*config.n];
+            Ey = (x1-x)*(y1-y)*electric_field[1][x0+(config.n-1)/2 + (y0+(config.n-1)/2)*config.n]
+               + (x1-x)*(y-y0)*electric_field[1][x0+(config.n-1)/2 + (y1+(config.n-1)/2)*config.n]
+               + (x-x0)*(y1-y)*electric_field[1][x1+(config.n-1)/2 + (y0+(config.n-1)/2)*config.n]
+               + (x-x0)*(y-y0)*electric_field[1][x1+(config.n-1)/2 + (y1+(config.n-1)/2)*config.n];
+        } else {
+            Ex = Ey = 0.;
         }
-        if (abs(x0) <= (config.n - 1)/2 && abs(y1) <= (config.n - 1)/2){
-            Ex01 = electric_field[0][x0+(config.n-1)/2 + (y1+(config.n-1)/2)*config.n];
-            Ey01 = electric_field[1][x0+(config.n-1)/2 + (y1+(config.n-1)/2)*config.n];
-        }
-        if (abs(x1) <= (config.n - 1)/2 && abs(y0) <= (config.n - 1)/2){
-            Ex10 = electric_field[0][x1+(config.n-1)/2 + (y0+(config.n-1)/2)*config.n];
-            Ey10 = electric_field[1][x1+(config.n-1)/2 + (y0+(config.n-1)/2)*config.n];
-        }
-        if (abs(x1) <= (config.n - 1)/2 && abs(y1) <= (config.n - 1)/2){
-            Ex11 = electric_field[0][x1+(config.n-1)/2 + (y1+(config.n-1)/2)*config.n];
-            Ey11 = electric_field[1][x1+(config.n-1)/2 + (y1+(config.n-1)/2)*config.n];
-        }
-
-        double Ex = Ex00*(x1-x)*(y1-y) + Ex01*(x1-x)*(y-y0) + Ex10*(x-x0)*(y1-y) + Ex11*(x-x0)*(y-y0);
-        double Ey = Ey00*(x1-x)*(y1-y) + Ey01*(x1-x)*(y-y0) + Ey10*(x-x0)*(y1-y) + Ey11*(x-x0)*(y-y0);
 
         particles[2*ii]   += config.mu*Ex + config.sigma*random.gauss(0., 1.);
         particles[2*ii+1] += config.mu*Ey + config.sigma*random.gauss(0., 1.);
     }
 
-    int k = 0;
     for(long unsigned int ii = 0; ii < particles.size()/2; ii++){
-        double x = particles[2*(ii-k)]/config.l;
-        double y = particles[2*(ii-k)+1]/config.l;
-        int x0 = floor(x);
-        int x1 = x0 + 1;
-        int y0 = floor(y);
-        int y1 = y0 + 1;
+        x = particles[2*(ii-k)]/config.l;
+        y = particles[2*(ii-k)+1]/config.l;
+        x0 = floor(x);
+        x1 = x0 + 1;
+        y0 = floor(y);
+        y1 = y0 + 1;
 
-        std::vector<int> neighbors;
-        if (abs(x0) <= (config.n - 1)/2 && abs(y0) <= (config.n - 1)/2)
-            neighbors.push_back(x0+(config.n-1)/2 + (y0+(config.n-1)/2)*config.n);
-        if (abs(x0) <= (config.n - 1)/2 && abs(y1) <= (config.n - 1)/2)
-            neighbors.push_back(x0+(config.n-1)/2 + (y1+(config.n-1)/2)*config.n);
-        if (abs(x1) <= (config.n - 1)/2 && abs(y0) <= (config.n - 1)/2)
-            neighbors.push_back(x1+(config.n-1)/2 + (y0+(config.n-1)/2)*config.n);
-        if (abs(x1) <= (config.n - 1)/2 && abs(y1) <= (config.n - 1)/2)
-            neighbors.push_back(x1+(config.n-1)/2 + (y1+(config.n-1)/2)*config.n);        
+        liquid = 1;
+        V_new = 1.;
+        if (((1-2*std::signbit(x0))*(x0+x1) <= config.n - 2) && ((1-2*std::signbit(y0))*(y0+y1) <= config.n - 2)){
 
-        bool liquid = 1;
-        double V_new = 1.;
-        for (auto &ii : neighbors){
-            liquid &= domain[ii];
-            V_new *= phi[ii];
-        }
+            neighbors[0] = x0+(config.n-1)/2 + (y0+(config.n-1)/2)*config.n;
+            neighbors[1] = x0+(config.n-1)/2 + (y1+(config.n-1)/2)*config.n;
+            neighbors[2] = x1+(config.n-1)/2 + (y0+(config.n-1)/2)*config.n;
+            neighbors[3] = x1+(config.n-1)/2 + (y1+(config.n-1)/2)*config.n;
 
-        if (!liquid){
-            if (V_new == 0) {
-                for (auto &ii : neighbors){
-                    domain[ii] = 0;
-                    phi[ii] = 0.0;
-                }
-                particles.erase(particles.begin()+2*(ii-k), particles.begin()+2*(ii-k)+1);
-                k += 1;
-            } else {
-                particles[2*(ii-k)] = particles_old[2*ii];
-                particles[2*(ii-k)+1] = particles_old[2*ii+1];
+            for (auto &ii : neighbors){
+                liquid &= domain[ii];
+                V_new *= phi[ii];
             }
+
+            if (!liquid){
+                if (V_new == 0) {
+                    for (auto &ii : neighbors){
+                        domain[ii] = 0;
+                        phi[ii] = 0.0;
+                    }
+                    particles.erase(particles.begin()+2*(ii-k), particles.begin()+2*(ii-k)+1);
+                    k += 1;
+                } else {
+                    particles[2*(ii-k)] = particles_old[2*ii];
+                    particles[2*(ii-k)+1] = particles_old[2*ii+1];
+                }
+            }
+        } else {
+            particles[2*(ii-k)] = particles_old[2*ii];
+            particles[2*(ii-k)+1] = particles_old[2*ii+1];
         }
     }
 
