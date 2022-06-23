fde7005899c5d1c79fcbc19957cd159a2d9bb667
cdelv <cdelv@unal.edu.co>
2022-06-22 22:14:47 -0500

    script para medir dimension fractal

---
+++ settings/fractal_dim.py
@@ -0,0 +1,46 @@
+from tools import *
+import os
+import matplotlib.pyplot as plt
+import porespy as ps
+from scipy import stats
+import numpy as np
+
+ps.visualization.set_mpl_style()
+
+
+def main():
+    #Parameters of the program
+    config = Config()
+
+    #Load last data point
+    path = 'results/data'
+    n=len(next(os.walk(path))[1])-1
+
+    fields, particles, bit_map = load_data(config,n)
+    bit_map = 1 - np.reshape(bit_map, (-1, config.n))
+
+    plt.imshow(bit_map)
+    plt.show()
+
+    data = ps.metrics.boxcount(bit_map)
+
+    result = stats.linregress(np.log10(data.size),np.log10(data.count))
+    print('Fractial dimention = {:.4f} +- {:.4f}'.format(result.slope, result.stderr))
+    label = r'$d=10^b N^d$''\n'r'$d$''={:.3f}±{:.3f} \n b={:.3f}±{:.3f} \n r={:.5f}'.format(result.slope, result.stderr, result.intercept, result.intercept_stderr, result.rvalue)
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
+    ax1.set_yscale('log')
+    ax1.set_xscale('log')
+    ax1.set_xlabel('box edge length')
+    ax1.set_ylabel('number of boxes spanning phases')
+    ax2.set_xlabel('box edge length')
+    ax2.set_ylabel('slope')
+    ax2.set_xscale('log')
+    ax1.plot(data.size, data.count,'-o')
+    ax1.plot(data.size,np.power(10,result.intercept)*np.power(data.size,result.slope),color='red',label=label)
+    ax2.plot(data.size, data.slope,'-o')
+    ax1.legend()
+    plt.show()
+
+if __name__ == '__main__':
+    main()
