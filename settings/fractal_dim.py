eed58d40d11f9cf3e136abbdb98c8e31c99064bc
cdelv <cdelv@unal.edu.co>
2022-06-23 09:46:00 -0500

    .

--- settings/fractal_dim.py
+++ settings/fractal_dim.py
@@ -15,26 +15,30 @@ def main():
 
     fields, particles, bit_map = load_data(config,n)
     bit_map = 1 - np.reshape(bit_map, (-1, config.n))
+    image = np.array(bit_map * 255, dtype = np.uint8)
 
-    data = ps.metrics.boxcount(bit_map)
+    #plt.imshow(image)
+    #plt.show()
 
-    result = stats.linregress(np.log10(data.size),np.log10(data.count))
-    print('Fractial dimention = {:.4f} +- {:.4f}'.format(result.slope, result.stderr))
-    label = r'$N=10^b L^d$''\n'r'$d$''={:.3f}±{:.3f} \n b={:.3f}±{:.3f} \n r={:.5f}'.format(result.slope, result.stderr, result.intercept, result.intercept_stderr, result.rvalue)
+    data = ps.metrics.boxcount(image,bins=np.array([6,8,12,16,20,25,32,44,50]))
 
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
-    ax1.set_yscale('log')
-    ax1.set_xscale('log')
-    ax1.set_xlabel('box edge length')
-    ax1.set_ylabel('number of boxes spanning phases')
-    ax2.set_xlabel('box edge length')
-    ax2.set_ylabel('slope')
-    ax2.set_xscale('log')
-    ax1.plot(data.size, data.count,'-o')
-    ax1.plot(data.size,np.power(10,result.intercept)*np.power(data.size,result.slope),color='red',label=label)
-    ax2.plot(data.size, data.slope,'-o')
+    L = np.log10(data.size)
+    N = np.log10(data.count)
+
+    result = stats.linregress(L,N)
+    print('Fractial dimention = {:.4f} +- {:.4f}'.format(-result.slope, result.stderr))
+    
+    '''
+    label = r'$log(N)= D log(L)+b$''\n'r'$D$''={:.3f}±{:.3f} \n b={:.3f}±{:.3f} \n r={:.5f}'.format(result.slope, result.stderr, result.intercept, result.intercept_stderr, result.rvalue)
+    fig, ax1 = plt.subplots(1, 1, figsize=(5, 5))
+    ax1.set_xlabel('log(box size)')
+    ax1.set_ylabel('log(count)')
+    ax1.plot(L, N,'-o')
+    ax1.plot(L,result.slope*L+result.intercept,color='red',label=label)
     ax1.legend()
+
     plt.show()
+    '''
 
 if __name__ == '__main__':
     main()
