334803d977280133b33a19de2fb910bdeaae366f
cdelv <cdelv@unal.edu.co>
2022-07-02 12:44:01 -0500

    arreglo script de bitmaps

--- settings/image_bitmap_extractor.py
+++ settings/image_bitmap_extractor.py
@@ -7,16 +7,16 @@ from PIL import Image, ImageChops
 
 def main():
     #Cargar Imagen
-    image = cv2.imread('test.jpeg')
+    image = cv2.imread('test1.jpg')
     plt.imshow(image)
     plt.show()
 
     #Para cortar la imagen
     '''
-    cut_x0 =
-    cut_xf =
-    cut_y0 =
-    cut_yf = 
+    cut_x0 = 300
+    cut_xf = 2850
+    cut_y0 = 700
+    cut_yf = 3280
     image = image[cut_y0:cut_yf, cut_x0:cut_xf]
     plt.imshow(image)
     plt.show()
@@ -24,13 +24,16 @@ def main():
 
     #Extraer contorno
     image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    image = cv2.GaussianBlur(image,(3,3),1000) 
+    image = cv2.GaussianBlur(image,(3,3), 2*cv2.BORDER_DEFAULT)
+    image = cv2.convertScaleAbs(image, alpha=2.6, beta=50) 
+    image = cv2.equalizeHist(image)
+
     flag, image = cv2.threshold(image, 40, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
                                        #Mover ese 40 según sea necesario
 
     #Quitar puntitos
-    image = cv2.GaussianBlur(image,(3,3),1000)
-    flag, image = cv2.threshold(image, 254, 255, cv2.THRESH_BINARY)
+    #image = cv2.GaussianBlur(image,(9,9), cv2.BORDER_DEFAULT)
+    #flag, image = cv2.threshold(image, 254, 255, cv2.THRESH_BINARY)
 
     plt.imshow(image)
     plt.show()
